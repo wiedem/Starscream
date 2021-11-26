f94fe686a9cabbce31645adf9d2181e6e2778fb7
Holger Wiedemann <holger.wiedemann@diva-e.com>
2021-11-26 19:53:13 +0100

    Cleanups.

--- Sources/Starscream.h
+++ Sources/Starscream.h
@@ -13,7 +13,3 @@ FOUNDATION_EXPORT double StarscreamVersionNumber;
 
 //! Project version string for Starscream.
 FOUNDATION_EXPORT const unsigned char StarscreamVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Starscream/PublicHeader.h>
-
-
